f9ff2b198f26e3b5924ed85c36ee96ada9665f1a
R13 <rbentos@yahoo.com>
2023-05-20 01:35:16 -0300

    World bounds: 8 x 8 Chambers

--- MUD2/packages/client/src/layers/phaser/constants.ts
+++ MUD2/packages/client/src/layers/phaser/constants.ts
@@ -18,5 +18,19 @@ export enum Assets {
   Tileset = "Tileset",
 }
 
-export const TILE_HEIGHT = 32;
 export const TILE_WIDTH = 32;
+export const TILE_HEIGHT = 32;
+
+export const CHAMBER_ROWS = 8;
+export const CHAMBER_COLS = 8;
+
+export const WORLD_WIDTH = CHAMBER_COLS * 20 * TILE_WIDTH;
+export const WORLD_HEIGHT = CHAMBER_ROWS * 20 * TILE_HEIGHT;
+
+
+export const BOUNDS = {
+  x: -WORLD_WIDTH / 2,
+  y: -WORLD_HEIGHT / 2,
+  width: WORLD_WIDTH,
+  height: WORLD_HEIGHT,
+}
