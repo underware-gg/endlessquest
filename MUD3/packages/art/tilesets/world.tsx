cd1c410f354fdbe9f83ac9c405007d8669e2bbb7
R13 <rbentos@yahoo.com>
2023-07-07 14:25:07 -0300

    MUD3: Replaced default art package

--- MUD3/packages/art/tilesets/world.tsx
+++ MUD3/packages/art/tilesets/world.tsx
@@ -1,19 +1,324 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.0" name="world" tilewidth="32" tileheight="32" tilecount="3" columns="3">
- <image source="images/world.png" width="96" height="32"/>
+<tileset version="1.10" tiledversion="1.10.1" name="world" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="images/world.png" width="256" height="256"/>
  <tile id="0">
   <properties>
-   <property name="name" value="Grass"/>
+   <property name="name" value="BG0"/>
   </properties>
  </tile>
  <tile id="1">
   <properties>
-   <property name="name" value="Mountain"/>
+   <property name="name" value="BG1"/>
   </properties>
  </tile>
  <tile id="2">
   <properties>
-   <property name="name" value="Forest"/>
+   <property name="name" value="BG2"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="name" value="BG3"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="name" value="BG4"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="name" value="BG5"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="name" value="BG6"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="name" value="BG7"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="name" value="Rock1"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="name" value="Rock2"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="name" value="Rock3"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="name" value="Rock4"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="name" value="Rock5"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="name" value="Rocks1"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="name" value="Rocks2"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="name" value="Rocks3"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="name" value="Moss1"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="name" value="Moss2"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="name" value="Moss3"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="name" value="Moss4"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="name" value="Moss5"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="name" value="Mosses1"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="name" value="Mosses2"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="name" value="Mosses3"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="name" value="Tree1"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="name" value="Tree2"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="name" value="Foliage1"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="name" value="Foliage2"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="name" value="Foliage3"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="name" value="Foliage4"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="name" value="Foliage5"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="name" value="Foliage6"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="name" value="Grass1"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="name" value="Grass2"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="name" value="Grass3"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="name" value=""/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="name" value=""/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="name" value=""/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="name" value=""/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="name" value=""/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="name" value="Water1"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="name" value="Water2"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="name" value="Water3"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="name" value="Water4"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="name" value="Floater1"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="name" value="Floater2"/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="name" value="Floater3"/>
+  </properties>
+ </tile>
+ <tile id="47">
+  <properties>
+   <property name="name" value="Floater4"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="name" value="AirPath1"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="name" value="AirPath2"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="name" value="AirPath3"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="name" value=""/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="name" value=""/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="name" value=""/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="name" value=""/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="name" value=""/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="name" value="FirePath1"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="name" value="FirePath2"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="name" value="Debris1"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="name" value="Debris2"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="name" value="Debris3"/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="name" value="Debris4"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="name" value=""/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="name" value=""/>
   </properties>
  </tile>
 </tileset>
